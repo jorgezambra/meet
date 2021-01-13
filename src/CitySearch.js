c631c5fc67a078d1da729281846aab5810c304cd
jorgezambra <jorgezambra@gmail.com>
2021-01-13 13:41:49 -0500

    task 4.7
    added alerts to number of events and city search in case user creates errors when searching for a city or inputs invalid number of events.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
 
     query: '',
     suggestions: [],
-    showSuggestions: false
+    showSuggestions: false,
+    infoText: ''
   }
 
   handleItemClicked = (suggestion) => {
@@ -19,20 +21,29 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
-  };
-
+    if (suggestions.length === 0) {
+     this.setState({
+       query: value,
+       infoText: 'We can not find the city you are looking for. Please try another city',
+     });
+   } else {
+     return this.setState({
+       query: value,
+       suggestions,
+       infoText:''
+     });
+   }
+ };
 
 
   render() {
     return (
       <div className="CitySearch">
+      <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
   state = {
     numberofevents: 32,
+    errorNumber: ''
   }
 
   changeEventNumber = (event) => {
+  if(event.target.value <= 32) {
     this.setState({
-      numberofevents: event.target.value
+      numberofevents: event.target.value,
+      errorNumber: ''
     });
+  } else {
+    return this.setState({
+        numberofevents: event.target.value,
+        errorNumber: 'Number must be between 1 and 32'
+      });
+    }
     this.props.updateEvents(null, event.target.value);
   };
 
@@ -21,6 +31,7 @@ class NumberOfEvents extends Component {
           value={this.state.numberofevents}
           onChange={this.changeEventNumber}
         ></input>
+        <ErrorAlert text={this.state.errorNumber} />
       </div>
     )
   }
