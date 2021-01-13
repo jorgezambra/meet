7fc1e1ddaaaf75603df69ebc178e9ac03a97413a
jorgezambra <jorgezambra@gmail.com>
2021-01-13 11:33:56 -0500

    Revert "task 4.6 error with react 17, no service worker"
    This reverts commit 9542a87f9063c51d725894b03272e0258dfed635.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-atatus.config('e15349f79e1945a1b46dd06351177248').install();
-atatus.notify(new Error('Test Atatus Setup'));
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
